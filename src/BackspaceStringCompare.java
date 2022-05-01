b5096fde433f6b6ec4ec6cbae21f5ab9d7073458
stilian-iliev <74832022+stilian-iliev@users.noreply.github.com>
2022-05-02 00:50:58 +0300

    daily

--- src/BackspaceStringCompare.java
+++ src/BackspaceStringCompare.java
@@ -1,27 +1,17 @@
 public class BackspaceStringCompare {
-    public static boolean backspaceCompare(String s, String t) {
-        StringBuilder ssb = new StringBuilder();
-        for (char c : s.toCharArray()) {
-            if (c == '#' && !ssb.isEmpty()){
-                ssb.deleteCharAt(ssb.length()-1);
-            } else if (c != '#'){
-                ssb.append(c);
-            }
-        }
-        StringBuilder tsb = new StringBuilder();
-        for (char c : t.toCharArray()) {
-            if (c == '#' && !tsb.isEmpty()){
-                tsb.deleteCharAt(tsb.length()-1);
-            } else if (c != '#'){
-                tsb.append(c);
-            }
-        }
-        return ssb.toString().equals(tsb.toString());
+    public boolean backspaceCompare(String s, String t) {
+        return build(s).equals(build(t));
     }
 
-    public static void main(String[] args) {
-        System.out.println(backspaceCompare("y#fo##f",
-                "y#f#o##f"
-        ));
+    public static String build(String s){
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (c == '#' && !sb.isEmpty()){
+                sb.deleteCharAt(sb.length()-1);
+            } else if (c != '#'){
+                sb.append(c);
+            }
+        }
+        return sb.toString();
     }
 }
